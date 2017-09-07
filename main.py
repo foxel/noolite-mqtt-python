c5c05bcd4d9a339013e87e204874b6c4e3888a32
Andrey F. Kupreychik <foxel@quickfox.ru>
2017-09-07 09:23:55 +0700

    Refactoring of main.py, fixed signed temperature handling

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import paho.mqtt.client as mqtt
 from noolite_serial import NooLiteSerial
 import re
 from time import sleep
+import signal
 
 PREFIX = 'foxhome/noolite'
 
@@ -23,86 +24,107 @@ F_COMMANDS = {
 }
 
 
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print('Connected with result code %d' % rc)
+class NooLiteMQTT:
+    def __init__(self, tty_name, mqtt_host, mqtt_port):
+        self._noo_serial = NooLiteSerial(tty_name)
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe([('%s/tx/#' % PREFIX, 0), ('%s/tx-f/#' % PREFIX, 0)])
+        self._mqtt_client = mqtt.Client()
+        self._mqtt_client.on_connect = self._on_connect
+        self._mqtt_client.on_message = self._on_message
 
+        self._mqtt_client.connect(mqtt_host, mqtt_port, 60)
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic+": "+str(msg.payload))
+        self._exit = False
 
-    tx_match = re.match('%s/tx/(\d+)' % PREFIX, msg.topic)
-    if tx_match:
-        ch = int(tx_match.group(1))
-        cmd = str(msg.payload)
-        if cmd in COMMANDS:
-            noo_serial.send_command(ch, COMMANDS[cmd], mode=0)
-            sleep(0.3)
+    def loop(self):
+        signal.signal(signal.SIGINT, self._interrupt_handler)
+        signal.signal(signal.SIGTERM, self._interrupt_handler)
 
-    tx_match = re.match('%s/tx-f/(\d+)' % PREFIX, msg.topic)
-    if tx_match:
-        ch = int(tx_match.group(1))
-        cmd = str(msg.payload)
-        if cmd in F_COMMANDS:
-            noo_serial.send_command(ch, F_COMMANDS[cmd], mode=2)
-            sleep(0.3)
+        while not self._exit:
+            # first receive packets from noolite serial
+            packets = self._noo_serial.receive()
+            for packet in packets:
+                self._on_packet(packet)
+            # here we run MQTT messages
+            self._mqtt_client.loop()
 
+    def _interrupt_handler(self, _signal, _frame):
+        print('Exiting loop...')
+        self._exit = True
 
-noo_serial = NooLiteSerial('/dev/ttyS0')
+    # The callback for when the client receives a CONNACK response
+    def _on_connect(self, client, _userdata, _flags, rc):
+        print('Connected with result code %d' % rc)
 
-mqtt_client = mqtt.Client()
-mqtt_client.on_connect = on_connect
-mqtt_client.on_message = on_message
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe([('%s/tx/#' % PREFIX, 0), ('%s/tx-f/#' % PREFIX, 0)])
 
-mqtt_client.connect('127.0.0.1', 1883, 60)
+    # The callback for when a PUBLISH message is received from the server.
+    def _on_message(self, _client, _userdata, msg):
+        print(msg.topic+": "+str(msg.payload))
 
-run = True
-while run:
-    packets = noo_serial.receive()
-    for packet in packets:
+        tx_match = re.match('%s/tx/(\d+)' % PREFIX, msg.topic)
+        if tx_match:
+            ch = int(tx_match.group(1))
+            cmd = str(msg.payload)
+            if cmd in COMMANDS:
+                self._noo_serial.send_command(ch, COMMANDS[cmd], mode=0)
+                sleep(0.3)
+
+        tx_match = re.match('%s/tx-f/(\d+)' % PREFIX, msg.topic)
+        if tx_match:
+            ch = int(tx_match.group(1))
+            cmd = str(msg.payload)
+            if cmd in F_COMMANDS:
+                self._noo_serial.send_command(ch, F_COMMANDS[cmd], mode=2)
+                sleep(0.3)
+
+    # The callback to call when packet received from noolite
+    def _on_packet(self, packet):
         mode = ord(packet[1])
         ch = ord(packet[4])
         cmd = ord(packet[5])
-        mqtt_client.publish(
+        self._mqtt_client.publish(
             '%s/echo/%d' % (PREFIX, ch),
             '[%s]' % ','.join([str(ord(b)) for b in packet])
         )
         if mode == 2 and cmd == 130:
             state = ord(packet[9]) & 0x0f
-            mqtt_client.publish(
+            self._mqtt_client.publish(
                 '%s/state-f/%d' % (PREFIX, ch),
                 'ON' if state > 0 else 'OFF',
                 retain=True
             )
         elif mode == 1:
             if cmd == 25 or cmd == 2:
-                mqtt_client.publish(
+                self._mqtt_client.publish(
                     '%s/switch/%d' % (PREFIX, ch),
                     'ON'
                 )
             elif cmd == 0:
-                state = ord(packet[9]) & 0x0f
-                mqtt_client.publish(
+                self._mqtt_client.publish(
                     '%s/switch/%d' % (PREFIX, ch),
                     'OFF'
                 )
-            elif cmd == 21: # temperature & humidity sensor
+            elif cmd == 21:  # temperature & humidity sensor
                 deci_temp = ord(packet[7]) | ((ord(packet[8]) & 0x0f) << 8)
+
+                # fix for signed 12-bit values
                 if deci_temp & 0x0800:
-                    deci_temp |= 0xf000
+                    from ctypes import c_int16
+                    deci_temp = c_int16(deci_temp | 0xf000).value
+
                 temp = deci_temp / 10.0
                 hum = ord(packet[9])
-                mqtt_client.publish(
+                self._mqtt_client.publish(
                     '%s/temperature/%d' % (PREFIX, ch),
                     '%.1f' % temp
                 )
-                mqtt_client.publish(
+                self._mqtt_client.publish(
                     '%s/humidity/%d' % (PREFIX, ch),
                     '%d' % hum
                 )
-    mqtt_client.loop()
+
+
+NooLiteMQTT('/dev/ttyS0', '127.0.0.1', 1883).loop()
